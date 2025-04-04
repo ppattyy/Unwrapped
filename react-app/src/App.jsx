2ee6340301da46b87b82b9158f4e41168aee1a54
Nathan <LilFuze9@gmail.com>
2025-04-04 17:41:57 -0400

    connect spotify button

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,16 +1,9 @@
 import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 import { Auth } from '@supabase/auth-ui-react';
-import {
-  // Import predefined theme
-  ThemeSupa,
-} from '@supabase/auth-ui-shared'
 import './App.css'
 
 const supabase = createClient("https://ohvjoekcangwhrpwqcpw.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9odmpvZWtjYW5nd2hycHdxY3B3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMxOTE0NzcsImV4cCI6MjA1ODc2NzQ3N30.NSkMSsN3lH0Bmgu6kuCinn0B7kY0L460D3Af142CEzc");
-
-const App = () => <Auth supabaseClient={supabase} />
-
 
 ```
   const [colors, setColors] = useState([]);
--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -2,11 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Login from './Login.jsx'
+import ConnectButton from './ConnectButton.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
+    <ConnectButton />
   </StrictMode>,
 )
 
