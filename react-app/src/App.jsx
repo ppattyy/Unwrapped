b93aa9852858c3f09e3ca77494792b640904c9dd
Nathan <LilFuze9@gmail.com>
2025-04-21 10:49:36 -0400

    getting the dev app to run

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -8,23 +8,7 @@ import Data from './Data.jsx';
 
 const supabase = createClient("https://ohvjoekcangwhrpwqcpw.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9odmpvZWtjYW5nd2hycHdxY3B3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMxOTE0NzcsImV4cCI6MjA1ODc2NzQ3N30.NSkMSsN3lH0Bmgu6kuCinn0B7kY0L460D3Af142CEzc");
 
-```
-  const [colors, setColors] = useState([]);
-  useEffect(() => {
-    getColors();
-  }, []);
-  async function getColors() {
-    const { data } = await supabase.from("sandbox").select();
-    setColors(data);
-  }
 
-  return (
-<ul>
-{colors.map((color) => (
-  <li key={color.id}>{color.color}</li>
-))}
-</ul>
-```
 
 function App() {
   return (
--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -9,7 +9,6 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ConnectButton />
     <BrowserRouter>
       <App />
     </BrowserRouter>
