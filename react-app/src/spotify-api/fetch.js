2dcd62e56fa994005c9a2e7472f54e90344b4415
Nathan <LilFuze9@gmail.com>
2025-04-24 11:22:50 -0400

    stuck in loading fix

--- react-app/src/spotify-api/fetch.js
+++ react-app/src/spotify-api/fetch.js
@@ -1,10 +1,18 @@
 export async function fetchProfile(token) {
-    const result = await fetch("https://api.spotify.com/v1/me", {
-        method: "GET", headers: { Authorization: `Bearer ${token}` }
-    });
+    try{
+        const result = await fetch("https://api.spotify.com/v1/me", {
+            method: "GET", headers: { Authorization: `Bearer ${token}` }
+        });
 
-    return await result.json();
-}
+        if (!response.ok) {
+            sessionStorage.setItem('loading', "false");
+
+        }
+        } catch (error) {
+            sessionStorage.setItem('loading', "false");
+        }
+        return await response.json();
+    }
 
 export async function fetchCurrentTrack(token) {
     const result = await fetch("https://api.spotify.com/v1/me/player/recently-played", {
@@ -15,20 +23,44 @@ export async function fetchCurrentTrack(token) {
 }
 
 export async function fetchTopTracks(token, limit, time_range) {
-    const result = await fetch(`https://api.spotify.com/v1/me/top/tracks?offset=0&limit=${limit}&time_range=${time_range}`, {
-        method: "GET", headers: { Authorization: `Bearer ${token}` }
-    });
+    try{
+        const result = await fetch(`https://api.spotify.com/v1/me/top/tracks?offset=0&limit=${limit}&time_range=${time_range}`, {
+            method: "GET", headers: { Authorization: `Bearer ${token}` }
+        });
 
-    return await result.json();
-}
+
+        if (!response.ok) {
+            sessionStorage.setItem('loading', "false");
+
+        }
+        } catch (error) {
+            sessionStorage.setItem('loading', "false");
+        }
+
+        return await response.json();
+    }
+    
 
 export async function fetchTopArtists(token, limit, time_range) {
-    const result = await fetch(`https://api.spotify.com/v1/me/top/artists?offset=0&limit=${limit}&time_range=${time_range}`, {
-        method: "GET", headers: { Authorization: `Bearer ${token}` }
-    });
+    try {
+        const response = await fetch(
+            `https://api.spotify.com/v1/me/top/artists?offset=0&limit=${limit}&time_range=${time_range}`,
+            {
+                method: "GET",
+                headers: { Authorization: `Bearer ${token}` }
+            }
+        );
 
-    return await result.json();
-}
+        if (!response.ok) {
+            sessionStorage.setItem('loading', "false");
+
+        }
+        } catch (error) {
+            sessionStorage.setItem('loading', "false");
+        }
+
+        return await response.json();
+    }
 
 export async function fetchArtist(token, id) {
     const result = await fetch(`https://api.spotify.com/v1/artists/${id}`, {
