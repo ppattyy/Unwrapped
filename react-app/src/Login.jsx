98c2064b5d5e208894b1a47fc21d304ba150b7f4
ppattyy <galla355@msu.edu>
2025-04-25 10:30:16 -0400

    more login changes

--- react-app/src/Login.jsx
+++ react-app/src/Login.jsx
@@ -58,8 +58,7 @@ const Login = () => {
     return (
       <>
         <div className="login-container">
-        <h1><span className='accent text-5xl font-bold'>Unwrapped</span></h1>
-        <h1 className="mt-12 font-extrabold">Sign {title || "in"}</h1>
+        <h1 className="mb-10"><span className='accent text-5xl font-extrabold'>Unwrapped</span></h1>
         <Auth
           supabaseClient={supabase}
           appearance={{ theme: ThemeSupa }}
